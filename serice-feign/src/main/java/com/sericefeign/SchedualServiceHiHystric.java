bc337439abe573dde6c8abd67d9a9f5b114e41af
BZPhaha <783535094@qq.com>
2018-07-09 14:03:44 +0800

    在feign中加断路器

--- serice-feign/src/main/java/com/sericefeign/SchedualServiceHiHystric.java
+++ serice-feign/src/main/java/com/sericefeign/SchedualServiceHiHystric.java
@@ -1,4 +1,15 @@
 package com.sericefeign;
 
-public class SchedualServiceHiHystric {
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
 }
