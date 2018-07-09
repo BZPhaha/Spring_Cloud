31b044e30c86a173ee8db6d193237de50c484a18
BZPhaha <7894@qq.com>
2018-07-09 13:15:34 +0800

    搭建基础spring-cloud 环境

---
+++ src/main/java/com/springcloud/spring_cloud_new/SpringCloudNewApplication.java
@@ -0,0 +1,12 @@
+package com.springcloud.spring_cloud_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudNewApplication.class, args);
+    }
+}
---
+++ testribbon/src/main/java/com/testribbon/control/HelloControl.java
@@ -0,0 +1,19 @@
+package com.testribbon.control;
+
+import com.testribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloControl {
+    @Autowired
+    HelloService helloService;
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String hi(@RequestParam String name){
+        System.out.println("访问到ctrl");
+        return helloService.hiService(name);
+    }
+}
